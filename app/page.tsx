48d88b76cecdb43d9ce9b2e9b60f754308019176
Sadaf672 <sadafkhankhan559@gmail.com>
2024-12-08 12:16:39 -0800

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import CarCard from '@/components/CarCard'
+import Catagore from '@/components/Catagore'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import Hero from '@/components/HrroSetion'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+     
+      
+
+      <Hero />
+      
+      <Catagore />
+      
+      
+      
+    </div>
+  )
+}
+
+export default Home
